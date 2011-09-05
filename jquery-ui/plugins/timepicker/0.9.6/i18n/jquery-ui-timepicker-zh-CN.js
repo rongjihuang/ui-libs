bd292bc3831ca0b6fe711040ad99735752d2ec1d
rongjihuang <rongjihuang@gmail.com>
2011-09-05 17:19:50 +0800

    添加jqueryUI timepicker插件

---
+++ jquery-ui/plugins/timepicker/0.9.6/i18n/jquery-ui-timepicker-zh-CN.js
@@ -0,0 +1,17 @@
+/* Spanish translation for the jQuery Timepicker Addon */
+/* Written by Ianaré Sévi */
+(function($) {
+	$.timepicker.regional['zh-CN'] = {
+		timeOnlyTitle: '选择时间',
+		timeText: '时间',
+		hourText: '时',
+		minuteText: '分',
+		secondText: '秒',
+		timezoneText: '时区',
+		currentText: '现在',
+		closeText: '关闭',
+		timeFormat: 'hh:mm',
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['zh-CN']);
+})(jQuery);
