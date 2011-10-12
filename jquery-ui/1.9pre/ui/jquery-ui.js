c4893ed3864667ba45f60a046a9cf5f8d2ac6477
rongjihuang <rongjihuang@gmail.com>
2011-10-12 18:36:31 +0800

    修改jqueryui的datepicker的zIndex,避免bctabs内的日历显示不了

--- jquery-ui/1.9pre/ui/jquery-ui.js
+++ jquery-ui/1.9pre/ui/jquery-ui.js
@@ -8499,7 +8499,7 @@ $.extend(Datepicker.prototype, {
 						width: inst.dpDiv.outerWidth(), height: inst.dpDiv.outerHeight()});
 				}
 			};
-			inst.dpDiv.zIndex($(input).zIndex()+1);
+			inst.dpDiv.zIndex($(input).zIndex()+10000);
 			$.datepicker._datepickerShowing = true;
 
 			// DEPRECATED: after BC for 1.8.x $.effects[ showAnim ] is not needed
